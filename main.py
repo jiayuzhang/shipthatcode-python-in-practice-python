3a2f303794b6329d541874e97c140fb6509e4076
Jiayu Zhang <jiayuz@uber.com>
2026-07-25 11:59:01 -0400

    04 done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-n = int(input())
-nums = [int(input()) for _ in range(n)]
+# n = int(input())
+# nums = [int(input()) for _ in range(n)]
 # # Find and print the second largest unique value
 
 # nums = list(set(nums))
@@ -13,10 +13,17 @@ nums = [int(input()) for _ in range(n)]
 #     print(n)
 
 
-def min_max(numbers) -> tuple[int, int]:
-    return min(numbers), max(numbers)
+# def min_max(numbers) -> tuple[int, int]:
+#     return min(numbers), max(numbers)
 
+# mn, mx = min_max(nums)
+# print(mn)
+# print(mx)
 
-mn, mx = min_max(nums)
-print(mn)
-print(mx)
+text = input()
+tokens = text.split(" ")
+counter = {}
+for token in tokens:
+    counter[token] = counter.get(token, 0) + 1
+for key, value in counter.items():
+    print(key, value)
