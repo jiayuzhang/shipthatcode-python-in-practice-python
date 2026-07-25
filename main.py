be263d278896e4eaee248bd7e32eb5e40a3b484a
Jiayu Zhang <jiayuz@uber.com>
2026-07-24 20:44:08 -0400

    02 done

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 n = int(input())
 nums = [int(input()) for _ in range(n)]
-# Find and print the second largest unique value
+# # Find and print the second largest unique value
 
-nums = list(set(nums))
-nums.sort()
-print(nums[-2])
+# nums = list(set(nums))
+# nums.sort()
+# print(nums[-2])
+
+res = [x * 2 for x in nums if x % 2 == 0]
+for n in res:
+    print(n)
